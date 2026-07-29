397a3831fae6c1ccb68f04e4e168d367e2d40392
Katsuyuki Omuro <komuro@fastly.com>
2026-07-30 00:23:54 +0900

    Updates for release

--- starter-kits/javascript/auth/src/lib/util.js
+++ starter-kits/javascript/auth/src/lib/util.js
@@ -6,3 +6,13 @@ export function generateRandomStr (length) {
     .join('')
     .slice(0, length)
 }
+
+// Returns true if the given path is safe to use as a same-origin redirect
+// target, i.e. it can't be interpreted as a protocol-relative or malformed
+// URL pointing off-site (e.g. "//evil.com/phish").
+export function isSafeRedirectTarget (path) {
+  return path.startsWith('/') &&
+    !path.startsWith('//') &&
+    !path.includes('\\') &&
+    ![...path].some(c => c.charCodeAt(0) < 0x20 || c.charCodeAt(0) === 0x7f)
+}
